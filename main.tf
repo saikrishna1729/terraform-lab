bdcbe923e9cfce5a50253b7ff602d6d42d7b0247
saikrishna <saikrishna.nagisetti@gmail.com>
2023-02-07 21:55:19 +0800

    Initial commit

---
+++ main.tf
@@ -0,0 +1,67 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  subnet_id = aws_subnet.my_subnet.id
+
+  tags = {
+    Name = var.VMname
+  }
+}
+
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = var.VPCName
+  }
+}
+
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "172.16.10.0/24"
+  availability_zone = var.AZ
+
+  tags = {
+    Name = var.SNName
+  }
+}
+
+resource "aws_security_group" "SG" {
+  name        = "allow_all"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.my_vpc
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_all"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "VMName" {
+    default = "Test-VM"
+}
+
+variable "VPCName" {
+    default = "Test-VPC"
+}
+
+variable "AZ" {
+    default = "ap-southeast-1a"
+  
+}
+
+variable "SNName" {
+    default = "Test-Subnet"
+  
+}
